888ce5abdbb740532fb48856e81501bea23533de
test1 <dzhuliayakuheva@gmail.com>
2024-11-17 11:58:45 +0300

    исправлены ошибки, программа работает

--- Dogs.py
+++ Dogs.py
@@ -4,9 +4,6 @@ import requests
 from PIL import Image, ImageTk
 from io import BytesIO
 
-from bottle import response
-from pyexpat.errors import messages
-from pygame.examples.cursors import image
 
 
 def get_dog_image():
@@ -14,7 +11,7 @@ def get_dog_image():
         response=requests.get("https://dog.ceo/api/breeds/image/random")# функция response будет JSON
         response.raise_for_status()
         data=response.json()
-        return data("message")#message отражается на сайте с которого мы взяли ссылку
+        return data["message"]#message отражается на сайте с которого мы взяли ссылку
     except Exception as e:
         mb.showerror("ошибка", f"возникла ошибка {e} при запросе к API")
         return None
@@ -28,8 +25,7 @@ def show_image():
             img_data=BytesIO(response.content)
             img=Image.open(img_data)
             img.thumbnail((300,300))# через запятую, т.к. это кортеж
-            img=ImageTk.PhotoImage()
-
+            img=ImageTk.PhotoImage(img)
             label.config(image=img)
             label.image=img
         except Exception as e:
